a1639b662945b7ce2d87e47bfbc9eb0ffaa17b2b
Cauan Mengali <cauanmengali10@gmail.com>
2026-06-09 19:08:27 -0300

    adicionado tarefa

---
+++ produdoro/src/main/java/dev/wakandaacademy/produdoro/tarefa/domain/StatusTarefa.java
@@ -0,0 +1,9 @@
+package dev.wakandaacademy.produdoro.tarefa.domain;
+
+
+public enum StatusTarefa {
+    A_FAZER, CONCLUIDA
+
+
+
+}
---
+++ produdoro/src/main/java/dev/wakandaacademy/produdoro/tarefa/domain/Tarefa.java
@@ -0,0 +1,32 @@
+package dev.wakandaacademy.produdoro.tarefa.domain;
+
+
+import lombok.*;
+import org.springframework.boot.jackson.JacksonMixin;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.UUID;
+
+@Builder
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Document(collection = "Tarefa")
+@Getter
+public class Tarefa {
+
+    @Id
+    private UUID idTarefa;
+    private String nome;
+    private String descricao;
+    @Indexed
+    private UUID idUsuario;
+    @Indexed
+    private UUID idArea;
+    @Indexed
+    private UUID idProjeto;
+    @Builder.Default
+    private StatusTarefa statusTarefa = StatusTarefa.A_FAZER;
+
+}
