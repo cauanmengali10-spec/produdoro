dff4ecbeb447763f9225cfd805e59e15882b8a48
Cauan Mengali <cauanmengali10@gmail.com>
2026-06-14 11:49:20 -0300

    adicionado handler e springdoc(swagger)

---
+++ produdoro/src/main/java/dev/wakandaacademy/produdoro/handler/HandlerGlobalException.java
@@ -0,0 +1,28 @@
+package dev.wakandaacademy.produdoro.handler;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class HandlerGlobalException {
+
+    @ExceptionHandler(ApiException.class)
+    public ResponseEntity<ErrorApiResponse>apiResponseResponseEntity(ApiException apiException){
+        return apiException.errorApiResponseResponseEntity();
+    }
+
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorApiResponse>handlerGenericException(Exception exception){
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ErrorApiResponse.builder()
+                        .message("INTERAL SERVER ERROR")
+                        .details("POR FAVOR INFORME AO ADMINISTRADOR DO SISTEMA")
+                        .build());
+    }
+
+}
